0d2d780c19916ebf75116217d271d06bb20eec14
tamsdomain\upasha <upasha@tams.com>
2015-03-04 15:02:11 -0800

    005 changed service path to dev for now

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -1,6 +1,7 @@
 //This is a configuration file for all hard-coded values in a specific environment
 
 var Configs = {
-	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/",
+//	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/",
+	"ServiceRootUrl": "http://tusspdev1wfe5/VirtualApps/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/",
 	"SharePointRootUrl" : "https://intranet.tams.com/"
 };
