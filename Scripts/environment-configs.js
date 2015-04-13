95b7bf3596d50cf0fd21dbded1836c619e0df525
tamsdomain\upasha <upasha@tams.com>
2015-04-13 15:14:46 -0700

    ver 0.93 to qa B

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -2,7 +2,7 @@
 
 var Configs = {
 
-//	"ServiceRootUrl": "https://intranetqa.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/","SharePointRootUrl" : "https://intranetqa.tams.com/"
-	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/","SharePointRootUrl" : "https://intranet.tams.com/"
+	"ServiceRootUrl": "https://intranetqa.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/","SharePointRootUrl" : "https://intranetqa.tams.com/"
+//	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/","SharePointRootUrl" : "https://intranet.tams.com/"
 //	"ServiceRootUrl": "http://tusspdev1wfe1/VirtualApps/BUSOPSWebs/TAMS.BUSOPS.BillTrak.MobileSVC/","SharePointRootUrl" : "http://tusspdev1wfe1/"
 };
