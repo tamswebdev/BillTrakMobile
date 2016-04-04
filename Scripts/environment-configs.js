3571df5122b368a89fddef63cc044b02fdf88625
GenreX <genrex@rocketmail.com>
2016-04-04 10:06:19 -0700

    Build to send for QA

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -3,12 +3,12 @@
 var Configs = {
 
 
-//	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/BUSOPSWebs/IPMMobileServiceQA/","SharePointRootUrl" : "https://intranet.tams.com/", "ConfidenceLevel" : 45
+	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/BUSOPSWebs/IPMMobileServiceQA/","SharePointRootUrl" : "https://intranet.tams.com/", "ConfidenceLevel" : 45
 
 
 //	"ServiceRootUrl": "http://tusspdev1wfe1/VirtualApps/tams.busops/IPMMobileService/","SharePointRootUrl" : "http://tusspdev1wfe1/", "ConfidenceLevel" : 45
 
-	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/BUSOPSWebs/IPMMobileService/","SharePointRootUrl" : "https://intranet.tams.com/", "ConfidenceLevel" : 45
+//	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/BUSOPSWebs/IPMMobileService/","SharePointRootUrl" : "https://intranet.tams.com/", "ConfidenceLevel" : 45
 };
 
 
