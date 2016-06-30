fa490266c75c6001b248a8108e95a17ffcce592b
GenreX <genrex@rocketmail.com>
2016-06-30 12:20:14 -0700

    Version 2.7.1 Prod with touch ID and SRD change email

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -6,7 +6,7 @@ var Configs = {
 //	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/BUSOPSWebs/IPMMobileServiceQA/","SharePointRootUrl" : "https://intranet.tams.com/", "ConfidenceLevel" : 45
 
 
-//	"ServiceRootUrl": "http://tusspdev1wfe1/VirtualApps/tams.busops/IPMMobileService/","SharePointRootUrl" : "http://tusspdev1wfe1/", "ConfidenceLevel" : 45
+//	"ServiceRootUrl": "http://tusspdev1wfe1/VirtualApps/tams.busops/IPMMobileService/","SharePointRootUrl" : "http://tusspdev1wfe1/", "ConfidenceLevel" : 15
 
 	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/BUSOPSWebs/IPMMobileService/","SharePointRootUrl" : "https://intranet.tams.com/", "ConfidenceLevel" : 45
 };
